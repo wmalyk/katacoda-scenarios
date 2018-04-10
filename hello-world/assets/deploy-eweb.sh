344c46dee6048bf56dacf86a59b121f745da631c
wmalyk <bill@enterpriseweb.com>
2018-04-09 22:00:11 -0400

    Update deploy-eweb.sh

--- hello-world/assets/deploy-eweb.sh
+++ hello-world/assets/deploy-eweb.sh
@@ -4,4 +4,9 @@ echo "Downloading..."
 sleep 4s
 echo "Deploying..."
 sleep 2s
+echo "Removing Firewall for Katacoda Use..."
+sleep 1s
+ufw disable
+echo "Completing Deployment..."
+sleep 1s
 echo "Application Manager ready on port 8075"
