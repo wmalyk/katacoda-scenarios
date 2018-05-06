5abec985291ee4d4cf27422a24e6fb8975b6f2e5
wmalyk <bill@enterpriseweb.com>
2018-05-06 12:06:42 -0400

    Update deploy-eweb.sh

--- hello-world2/assets/deploy-eweb.sh
+++ hello-world2/assets/deploy-eweb.sh
@@ -1,5 +1,5 @@
 echo "Connecting to EWeb Servers..."
-ssh -fN -4 -p 13 wmalyk@99.252.137.208 -L 8078:localhost:8078
+ssh -fN -4 -p 13 wmalyk@99.252.137.208 -L 8048:localhost:8048
 echo "Downloading..."
 sleep 4s
 echo "Deploying..."
@@ -9,4 +9,4 @@ sleep 1s
 ufw disable
 echo "Completing Deployment..."
 sleep 1s
-echo "Service Manager ready on port 8078"
+echo "Service Manager ready on port 8048"
