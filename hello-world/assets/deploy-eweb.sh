dd777ee82dc7548481f9538c7f89728f31fd9b1f
wmalyk <bill@enterpriseweb.com>
2018-05-06 12:05:56 -0400

    Update deploy-eweb.sh

--- hello-world/assets/deploy-eweb.sh
+++ hello-world/assets/deploy-eweb.sh
@@ -1,5 +1,5 @@
 echo "Connecting to EWeb Servers..."
-ssh -fN -4 -p 13 wmalyk@99.252.137.208 -L 8075:localhost:8075
+ssh -fN -4 -p 13 wmalyk@99.252.137.208 -L 8045:localhost:8045
 echo "Downloading..."
 sleep 4s
 echo "Deploying..."
@@ -9,4 +9,4 @@ sleep 1s
 ufw disable
 echo "Completing Deployment..."
 sleep 1s
-echo "Application Manager ready on port 8075"
+echo "Application Manager ready on port 8045"
