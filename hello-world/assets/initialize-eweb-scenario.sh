07c5c5df62aaae0bfccaebafd5d2078088c06497
wmalyk <bill@enterpriseweb.com>
2018-05-14 11:07:51 -0400

    Update initialize-eweb-scenario.sh

--- hello-world/assets/initialize-eweb-scenario.sh
+++ hello-world/assets/initialize-eweb-scenario.sh
@@ -4,13 +4,9 @@ ssh -fN -4 -p 13 wmalyk@99.252.137.208 -L 8045:localhost:8045
 
 echo "Deploying Module..."
 
-echo 2
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://99.252.137.208:8045/api/initScenario1
 
 sleep 10s
-
-echo "Initializing Scenario..."
-
-sleep 2s
 
 echo "Removing Firewall for Katacoda Use..."
 
@@ -20,6 +16,6 @@ ufw disable
 
 echo "Completing Initialization..."
 
-sleep 1s
+sleep 2s
 
 echo "Application Manager ready on port 8045, and at: http://99.252.137.208:8045"
