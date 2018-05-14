b43b69ba640038e6ce82fb6bd12e426c8ea48c13
wmalyk <bill@enterpriseweb.com>
2018-05-14 09:46:14 -0400

    Create initialize-eweb-scenario.sh

---
+++ hello-world/assets/initialize-eweb-scenario.sh
@@ -0,0 +1,25 @@
+echo "Connecting to EWeb Servers..."
+
+ssh -fN -4 -p 13 wmalyk@99.252.137.208 -L 8045:localhost:8045
+
+echo "Deploying Module..."
+
+echo 2
+
+sleep 10s
+
+echo "Initializing Scenario..."
+
+sleep 2s
+
+echo "Removing Firewall for Katacoda Use..."
+
+sleep 1s
+
+ufw disable
+
+echo "Completing Initialization..."
+
+sleep 1s
+
+echo "Application Manager ready on port 8045, and at: 99.252.137.208:8045"
