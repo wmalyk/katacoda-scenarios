0077509682bdce0abc1125f58ecd37d3bf8e940e
wmalyk <bill@enterpriseweb.com>
2018-06-05 10:17:36 +0200

    Update initialize-eweb-scenario.sh

--- hello-world/assets/initialize-eweb-scenario.sh
+++ hello-world/assets/initialize-eweb-scenario.sh
@@ -1,10 +1,10 @@
 echo "Connecting to EWeb Servers..."
 
-ssh -fN -4 -p 13 wmalyk@99.252.137.208 -L 8045:localhost:8045
+ssh -fN -4 -p 13 wmalyk@99.234.141.68 -L 8045:localhost:8045
 
 echo "Deploying Module..."
 
-curl -H "Accept: application/json" -H "Content-Type: application/json" http://99.252.137.208:8045/api/initScenario1
+curl -H "Accept: application/json" -H "Content-Type: application/json" http://99.234.141.68:8045/api/initScenario1
 
 sleep 10s
 
@@ -18,4 +18,4 @@ echo "Completing Initialization..."
 
 sleep 2s
 
-echo "Application Manager ready on port 8045, and at: http://99.252.137.208:8045"
+echo "Application Manager ready on port 8045, and at: http://99.234.141.68:8045"
